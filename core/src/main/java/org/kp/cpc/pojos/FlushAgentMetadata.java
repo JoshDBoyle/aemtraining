bda479ff0184bc50b76020fc499c0ae5233eb42b
Josh Boyle <joshua.boyle@accenture.com>
2017-01-17 11:49:54 -0800

    Moar logging

--- core/src/main/java/org/kp/cpc/pojos/FlushAgentMetadata.java
+++ core/src/main/java/org/kp/cpc/pojos/FlushAgentMetadata.java
@@ -2,8 +2,13 @@ package org.kp.cpc.pojos;
 
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FlushAgentMetadata extends AgentMetadata {
+
+	Logger log = LoggerFactory.getLogger(FlushAgentMetadata.class);
+	
 	public FlushAgentMetadata(JSONObject json) {
 		try {
 			this.title = json.getString("title");
@@ -11,6 +16,8 @@ public class FlushAgentMetadata extends AgentMetadata {
 			this.agentId = json.getString("agentId");
 			this.paused = json.getBoolean("paused");
 			this.transportUri = json.getString("transportUri");
+			
+			log.error("YO JOSH HERE'S THE TRANSPORTURI THAT CAME OVER FROM PUBLISH FOR THIS FLUSH AGENT: " + this.transportUri);
 		} catch (JSONException e) {
 			
 		}
